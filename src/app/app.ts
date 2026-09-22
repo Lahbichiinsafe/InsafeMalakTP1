8ee930c615d764d1e50980a15079b5e472938f37
Insafe Lahbichi <lahbichiinsaf@gmail.com>
2026-09-22 16:38:01 +0200

    Footer complet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import {Footer} from './footer/footer';
 
 @Component({
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Footer],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
